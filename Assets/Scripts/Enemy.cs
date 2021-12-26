db30b771db033bd99a3e07b529e80a72ba1baf58
Ellen <lemifi@mail.ru>
2021-12-27 02:00:05 +0300

    added UI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddBoxCollider()
     {
-        
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/SelfDestructor.cs
+++ Assets/Scripts/SelfDestructor.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class SelfDestructor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoadFirstScene", 3f);
+        Destroy(gameObject, 5f);
     }
 
-    void LoadFirstScene()
+    // Update is called once per frame
+    void Update()
     {
-        SceneManager.LoadScene(1);
+        
     }
-
 }
